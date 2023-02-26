ae857e21687c2e47207143efd9cf5dcde5dc60fe
bengin34 <becaglar3434@gmail.com>
2023-02-26 23:35:42 +0300

    half

--- src/App.js
+++ src/App.js
@@ -1,17 +1,67 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
 
 function App() {
-const [name,setName] = useState( '' )
-const [list,setList] = useState( [] )
-const [isEditing, setEditing] = useState(false)
-const [editID, setEditID] = useState(null)
-const [alert,setAlert] = useState({show:false, msg:'',type: ''})
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ 
+    show: false,
+     msg: "", 
+     type: "" });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!name){
+      showAlert(true,'danger','Please enter value')
 
-  return <section className='section-center'>
+    }
+    else if(name && isEditing ){
+      //deal with edit
+    }
+    else{
+      showAlert()
+      const newItem={
+        id:new Date().getTime().toString() ,title:name,
+      }
+      setList([...list, newItem]);
+      setName('')
 
-  </section>
+    }
+
+  };
+
+  const showAlert = (show=false ,type="",msg="") => {
+    setAlert({show,type,msg})
+  } 
+
+  return (
+    <section className="section-center">
+      <form className="grocery-form" action="" onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert}  removeAlert={showAlert} /> }
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g. Study React Hooks"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && 
+      <div className="grocery-container">
+        <List  items={list} />
+        <button className="clear-btn">clear items</button>
+      </div>
+    }
+    </section>
+  );
 }
 
-export default App
+export default App;
